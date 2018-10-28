3096bf90fffd25c688b44a146db1311add391e3d
ahmadelsallab <ahmad.elsallab@gmail.com>
2018-10-28 13:08:16 +0200

    Add files via upload

---
+++ preprocess.py
@@ -0,0 +1,12 @@
+def preprocess(image, remove_lines=False):
+    
+    # Deskewing
+    
+    # Non-local Means (NLM)
+    
+    # Lines removal
+    if remove_lines:
+        # Contours and edges detection
+        # Apply lines removal rule
+        
+    return clean_image
